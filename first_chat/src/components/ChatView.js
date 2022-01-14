2431f97262fc921c0e92f9f45847e93b7b93a833
Mariusz Nieciecki <mariusz.nieciecki@o2.pl>
2022-01-14 16:42:14 +0100

    chat - first message recognize (bot/user)

--- first_chat/src/components/ChatView.js
+++ first_chat/src/components/ChatView.js
@@ -7,21 +7,61 @@ import { useEffect } from 'react';
 const ChatView = (props) =>{
     const [messages, setMessage] = React.useState({mess:[]});
 
-      const addMessage=(text)=>{
-        const newMess={
-            id: messages.mess.length +1,
-            text:text,
+    const addMessage=(text, isMyMessage)=>{
+        if(messages.mess.length>0){
+            console.log(messages.mess[messages.mess.length-1].isMyMessage);
+            if((messages.mess[messages.mess.length-1].isMyMessage===true && isMyMessage===true) || (messages.mess[messages.mess.length-1].isMyMessage===false && isMyMessage===false)){
+                const newMess={
+                    id: messages.mess.length +1,
+                    text:text,
+                    isMyMessage: isMyMessage,
+                    firstMessage: false,
+                }
+                setMessage(prevState=>({
+                    mess: [...prevState.mess, newMess]
+                }))
+            } else{
+                const newMess={
+                    id: messages.mess.length +1,
+                    text:text,
+                    isMyMessage: isMyMessage,
+                    firstMessage: true,
+                }
+                setMessage(prevState=>({
+                    mess: [...prevState.mess, newMess]
+                }))
+            }
         }
-        setMessage(prevState=>({
-          mess: [...prevState.mess, newMess]
-        }))
-      }
-
+        else{
+            const newMess={
+                id: messages.mess.length +1,
+                text:text,
+                isMyMessage: isMyMessage,
+                firstMessage: true,
+            }
+            setMessage(prevState=>({
+                mess: [...prevState.mess, newMess]
+            }))
+        }
+        
+    }
     function handleKeyPress(e){
         if(e.key === 'Enter'){
             const newMessage = e.target.value;
             if(newMessage!==""){
-                addMessage(newMessage)
+                addMessage(newMessage, true)
+                e.target.value="";
+            }
+            else{
+                alert("You cannot send an empty message");
+            }
+        }
+    }
+    function handleKeyPresse(e){
+        if(e.key === 'Enter'){
+            const newMessage = e.target.value;
+            if(newMessage!==""){
+                addMessage(newMessage, false)
                 e.target.value="";
             }
             else{
@@ -50,11 +90,13 @@ const ChatView = (props) =>{
             </div>
 
             <MyMessage messages={messages}></MyMessage>
+
             <div className="downBar">
                 <FontAwesomeIcon icon={faSmile} style={{color:  "grey"} }/>
                 <input type="text" className="message" placeholder="type something..." onKeyPress={handleKeyPress}/>
                 <FontAwesomeIcon icon={faPaperclip} style={{color:  "grey"} }/>
             </div>
+            <input type="text" className='bigione' placeholder="type something..." onKeyPress={handleKeyPresse}/>
             </div>
         </>
          )
--- first_chat/src/components/MyMessage.js
+++ first_chat/src/components/MyMessage.js
@@ -1,14 +1,41 @@
 import React from 'react';
-// import { useEffect } from 'react';
 
 const MyMessage = props => {
     const texts = [...props.messages.mess].map(mess=>(
-        <div className="myMessage" key={mess.id}>
-            <p>{mess.text}</p>
-        </div>
+        console.log(mess.firstMessage),
+        mess.isMyMessage===true ?
+            mess.firstMessage===true? 
+                <div className='myWrapper' key={mess.id}>
+                    <p>Zdjęcie</p>
+                    <div className="myMessage" >
+                        <p>{mess.text}</p>
+                    </div>
+                </div>
+            : 
+                <div className='myWrapper' key={mess.id}>
+                <div className="myMessage" >
+                    <p>{mess.text}</p>
+                    <p>{mess.firstMessage}</p>
+                </div>
+                </div>
+        : 
+            mess.firstMessage===true?
+                <div className='botWrapper' key={mess.id} >
+                    <p>Zdjęcie</p>
+                    <div className="botMessage">
+                        <p>{mess.text}</p>
+                    </div>      
+                </div>
+            :
+                <div className='botWrapper' key={mess.id} >
+                    <div className="botMessage">
+                        <p>{mess.text}</p>
+                    </div>      
+                </div>
     ));
     return(
         <>
+        {console.log(props.messages)}
            <div className='center'>
                <div className='myMessages'>{texts}</div>
             </div> 
