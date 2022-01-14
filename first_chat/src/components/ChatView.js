90609fa1d46229cf0e3aaaaad2699e3ed3160499
Mariusz Nieciecki <mariusz.nieciecki@o2.pl>
2022-01-14 10:10:00 +0100

    add messages - without key

--- first_chat/src/components/ChatView.js
+++ first_chat/src/components/ChatView.js
@@ -4,33 +4,32 @@ import { faSmile, faPaperclip, faArrowCircleLeft } from '@fortawesome/free-solid
 
 
 const ChatView = props =>{
-    let [text, setText] = React.useState([]);
-    const handlerOnChange=(e)=>{
-    }
-    const handleSubmit=(e)=>{
-        console.log(e.target.value);
-    }
+    let [state, setState] = React.useState("");
+
     function handleKeyPress(e){
         if(e.key === 'Enter'){
-            let newMessage = e.target.value;
-            console.log(newMessage);
-            setText(text.push(newMessage));
-            console.log("Log new text");
-            console.log(text);
-            // e.target.value="";
+            const newMessage = e.target.value;
+            setState(prev => [...prev, newMessage]);
+             e.target.value="";
         }
     }
+    const texts = [...state].map(text=>(
+        <div>
+            <p>{text}</p>
+        </div>
+    ));
+    
     return(
-        <>  
+        <>
             <div className='square'> 
             <div className="appBar">
                 <button onClick={props.onClick}> <FontAwesomeIcon icon={faArrowCircleLeft}/></button>
                 <p><span></span> JERONIMO</p>
             </div>
-            <div className="center">{text[0]}</div>
+            <div className="center">{texts}</div>
             <div className="downBar">
                 <FontAwesomeIcon icon={faSmile} style={{color:  "grey"} }/>
-                <input type="text" className="message" placeholder="type something..." onChange={handlerOnChange} onSubmit={handleSubmit} onKeyPress={handleKeyPress}/>
+                <input type="text" className="message" placeholder="type something..." onKeyPress={handleKeyPress}/>
                 <FontAwesomeIcon icon={faPaperclip} style={{color:  "grey"} }/>
             </div>
             </div>
